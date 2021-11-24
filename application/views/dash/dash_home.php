3709a147b5f1c0698ab35429e7aa836914b2d6be
Dana Sugu <dana.sugu@gmail.com>
2021-11-24 07:16:03 +0200

    Update dash_home.php

--- application/views/dash/dash_home.php
+++ application/views/dash/dash_home.php
@@ -31,8 +31,15 @@ $this->load->view('dash/inc/nav');
 <div class="panel panel-default">
   <div class="panel-headings">Employee Actions</div>
   <div class="list-group">
-    <a href="#" class="list-group-item">Add Employees</a>
+    <a href="#" class="list-group-item">Add Employee</a>
     <a href="#" class="list-group-item">Employees List</a>
+  </div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-headings">Jobs Actions</div>
+  <div class="list-group">
+    <a href="#" class="list-group-item">Add Job</a>
+    <a href="#" class="list-group-item">Jobs List</a>
   </div>
 </div>
 </div>
