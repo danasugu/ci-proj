ee211b2ce2dec54fe44db226907327b3a72049bd
Dana Sugu <dana.sugu@gmail.com>
2021-11-25 12:52:37 +0200

    Update add_job.php

--- application/views/dash/add_job.php
+++ application/views/dash/add_job.php
@@ -1,1 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+if( !$_SESSION['u_name'] ){
+  redirect('home', 'refresh');
+}
+?>
+<!-- Welcome, <?php echo $_SESSION['u_name']; ?>!-->
 
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" >
+
+    <title>Dashboard</title>
+  </head>
+  <body>
+<!--dash nav -->
+<?php
+$this->load->view('dash/inc/nav');
+ ?>
