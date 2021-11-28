728bbdf9219dd7ac7f3551694c8c132c97c50e8d
Dana Sugu <dana.sugu@gmail.com>
2021-11-28 08:09:22 +0200

    Update add_job.php

--- application/views/dash/add_job.php
+++ application/views/dash/add_job.php
@@ -40,32 +40,24 @@ $this->load->view('dash/inc/nav');
 					<div class="panel-heading">Add Jobs</div>
 					<div class="panel-body">
 						<?php
-						form_open('url','class="form-horizontal"');
+						form_open('jobs/add-job','class="form-horizontal"');
 						?>
 						<div class="form-group">
-							<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+							<label class="col-sm-2 control-label">Job Name</label>
 							<div class="col-sm-10">
-								<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+								<input type="text" class="form-control input-sm" placeholder="Job Name">
 							</div>
 						</div>
 						<div class="form-group">
-							<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+							<label class="col-sm-2 control-label">Job Description</label>
 							<div class="col-sm-10">
-								<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+								<input type="text" class="form-control input-sm" placeholder="Job Description">
 							</div>
 						</div>
+
 						<div class="form-group">
 							<div class="col-sm-offset-2 col-sm-10">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox"> Remember me
-									</label>
-								</div>
-							</div>
-						</div>
-						<div class="form-group">
-							<div class="col-sm-offset-2 col-sm-10">
-								<button type="submit" class="btn btn-default">Sign in</button>
+								<input type="submit" value="Submit">
 							</div>
 						</div>
 						<?php
